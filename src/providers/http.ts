b4156f65dd6200950cc0497b886534096951dff1
skala1988 <robert.kubisiak8@gmail.com>
2016-10-28 17:33:05 +0200

    http

--- src/providers/http.ts
+++ src/providers/http.ts
@@ -3,7 +3,7 @@ import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class HttpServiceTestTestasf {
+export class HttpService {
   constructor(public http: Http) {
     console.log('Hello Http Provider');
   }
