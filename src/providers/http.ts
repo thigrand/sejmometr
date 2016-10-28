0792b4704885865a12e123ca2b0bf8bf8a66920e
skala1988 <robert.kubisiak8@gmail.com>
2016-10-28 15:47:03 +0200

    revert test

--- src/providers/http.ts
+++ src/providers/http.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class HttpServiceTest2 {
+export class HttpService {
   constructor(public http: Http) {
     console.log('Hello Http Provider');
   }
