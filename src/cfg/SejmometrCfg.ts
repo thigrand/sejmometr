98adf6a5f6e9a987db5a4be5b3a023fa04e478f4
skala1988 <robert.kubisiak8@gmail.com>
2016-11-04 13:01:29 +0100

    SejmometrCfg sejmometr provider update

---
+++ src/cfg/SejmometrCfg.ts
@@ -0,0 +1,12 @@
+export class SejmometrCfg {
+  public actualCandence: string = '8';
+  public politicalPartiesClubsIds = {
+    KUKIZ: '13',
+    PIS: '2',
+    PSL: '3',
+    PO: '1',
+    NOWOCZESNA: '14',
+    NIEZRZESZENI: '7',
+    BRAK: '0'
+  };
+}
