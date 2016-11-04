9e6934c9f615c36a0f678443dfe7dd855941ee51
skala1988 <robert.kubisiak8@gmail.com>
2016-11-04 13:01:29 +0100

    SejmometrCfg sejmometr provider update

--- src/providers/sejmometr.ts
+++ src/providers/sejmometr.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
-import {Observable, Subject} from 'rxjs';
-import {DeputyDataHttpResponse} from '../interfaces/';
+import {Observable, Subject, Subscription} from 'rxjs';
+import {
+  DeputyDataHttpResponse,
+  SingleDeputyDataHttpResponse
+} from '../interfaces/';
 import {DeputiesService} from './deputies';
-import {SingleDeputyDataHttpResponse} from '../interfaces/deputies';
-import * as _ from 'lodash';
+import {SejmometrCfg} from '../cfg/';
 
 @Injectable()
 /**
@@ -11,41 +13,75 @@ import * as _ from 'lodash';
  * @class SejmometrService
  */
 export class SejmometrService {
-  public allDeputies: Array<SingleDeputyDataHttpResponse>;
-  private actualCandence: string = '8';
-  public deputiesIndexedByPP: Subject<Array<SingleDeputyDataHttpResponse>> = new Subject<Array<SingleDeputyDataHttpResponse>>();
+  private subscriptions: Array<Subscription> = [];
   private deputiesHttpResponse: Observable<DeputyDataHttpResponse>;
+
+  public allDeputies: Array<SingleDeputyDataHttpResponse>;
+  public deputiesIndexedByPP: Subject<Array<Array<SingleDeputyDataHttpResponse>>> = new Subject<Array<Array<SingleDeputyDataHttpResponse>>>();
   /**
    * @constructor
    * @param deputiesService DeputiesService provider
+   * @param sejmometrCfg Global Sejmometr cfg object
    */
   constructor(
-    private deputiesService: DeputiesService
+    private deputiesService: DeputiesService,
+    private sejmometrCfg: SejmometrCfg
   ) {
+    this.loadDeputiesHttpResponse();
+  }
+
+  public getDeputiesIndexedByPP(): Subject<Array<Array<SingleDeputyDataHttpResponse>>> {
+    this.ascertainDeputiesAvailable();
+    return this.deputiesIndexedByPP;
+  }
+
+  public getDeputiesByPP(club_id: string): Observable<DeputyDataHttpResponse> {
     let conditions = [];
-    conditions['poslowie.kadencja'] = [this.actualCandence];
-    this.deputiesHttpResponse = this.deputiesService.getDataFiltered({
+    conditions['poslowie.kadencja'] = [this.sejmometrCfg.actualCandence];
+    conditions['poslowie.klub_id'] = [club_id];
+    return this.deputiesService.getDataFiltered({
       conditions,
       limit: '500'
     });
-
   }
-  refreshDeputiesIndexedByPoliticalParty() {
-    this.deputiesHttpResponse.subscribe(deputyData => {
-      this.allDeputies = deputyData.Dataobject;
 
-      let res = [];
-      this.allDeputies.forEach(singleDeputy => {
-        if (!res[singleDeputy.data['sejm_kluby.nazwa']]) {
-          res[singleDeputy.data['sejm_kluby.nazwa']] = [];
-        }
-        // res[singleDeputy.data['poslowie.klub_id']].push(singleDeputy);
-        res[singleDeputy.data['sejm_kluby.nazwa']].push(singleDeputy);
+  private ascertainDeputiesAvailable(isReset: boolean = false) {
+    if (!this.allDeputies || isReset === true) {
+      this.resetArrays();
+      this.subscriptions['deputiesHttpResponse'] = this.deputiesHttpResponse.subscribe(deputyData => {
+        this.allDeputies = deputyData.Dataobject;
+        this.refreshDeputiesIndexedByPP();
       });
-      this.deputiesIndexedByPP.next(
-        res
-      );
-      console.log(this.deputiesIndexedByPP);
+    }
+  }
+
+  private resetArrays() {
+    this.allDeputies = [];
+    this.deputiesIndexedByPP.next([]);
+    if (this.subscriptions['deputiesHttpResponse']) {
+      this.subscriptions['deputiesHttpResponse'].unsubscribe();
+    }
+  }
+
+  private refreshDeputiesIndexedByPP() {
+    let res = [];
+    this.allDeputies.forEach(singleDeputy => {
+      if (!res[singleDeputy.data['sejm_kluby.nazwa']]) {
+        res[singleDeputy.data['sejm_kluby.nazwa']] = [];
+      }
+      res[singleDeputy.data['sejm_kluby.nazwa']].push(singleDeputy);
+    });
+    this.deputiesIndexedByPP.next(
+      res
+    );
+  }
+
+  private loadDeputiesHttpResponse() {
+    let conditions = [];
+    conditions['poslowie.kadencja'] = [this.sejmometrCfg.actualCandence];
+    this.deputiesHttpResponse = this.deputiesService.getDataFiltered({
+      conditions,
+      limit: '500'
     });
   }
 }
