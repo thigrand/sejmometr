f97024e3bd7cd6bd3709820dc07ae4e47e8ab270
skala1988 <robert.kubisiak8@gmail.com>
2016-11-10 16:39:10 +0100

    README.md + interface changes

--- src/interfaces/sejmometrProvider.ts
+++ src/interfaces/sejmometrProvider.ts
@@ -1,6 +1,13 @@
 import {Subject, Observable} from 'rxjs';
 import {SingleDeputyDataHttpResponse, DeputyDataHttpResponse} from './deputies';
 
+export interface DeputyExpenseArrayItem{
+  name: string;
+  club_id: string;
+  club_name: string;
+  spent: number;
+}
+
 export interface SejmometrSubjectsObj {
   allDeputies: Subject<Array<SingleDeputyDataHttpResponse>>;
   deputiesHttpResponse: Observable<DeputyDataHttpResponse>;
