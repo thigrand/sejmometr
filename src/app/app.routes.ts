f866a98ce650e23aa280cb1ebca197da7b7461ed
tstryjecki <thigrand@op.pl>
2017-01-18 18:22:15 +0100

    Preparing to github pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,8 @@ import { DeputyComponent } from './components/deputy/deputy.component';
 
 export const AppRoutes: Routes = [
   {path: '', component: DashboardComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'browser', component: ListComponent},
-  {path: 'browser/:id', component: ListComponent},
-  {path: 'deputy/:id', component: DeputyComponent},
+  {path: './dashboard', component: DashboardComponent},
+  {path: './browser', component: ListComponent},
+  {path: './browser/:id', component: ListComponent},
+  {path: './deputy/:id', component: DeputyComponent},
 ];
